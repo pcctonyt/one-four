b25577cf6ae8f74c91a2dd93407d7e29a119aae2
pcctonyt <104700781+pcctonyt@users.noreply.github.com>
2022-08-11 13:36:59 -0500

    My lesson answer L2 8/11/2022

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo192.svg";
+import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
@@ -10,10 +10,26 @@ const App = () => {
     license: "MIT",
   };
 
-  const AboutReact = (facts) =>
-    `Release date: ${facts.releaseDate}. Language: ${facts.releaseDate}. License: ${facts.license}.`;
+  const AboutReact = () => {
 
-  return <div className="container"></div>;
+    return `Release date: ${facts.releaseDate}. Language: ${facts.releaseDate}. License: ${facts.license}.`;
+  }
+  return(
+    <div className="container">
+      <img src="./logo.svg" alt="React logo" />
+      <h1>{name}</h1>
+      <div className="img-wrapper">
+          <img src="./logo192.png" alt="React logo .png" />
+      </div>
+      <ol>React Information for L2
+        <li>React favors the use of declarative,rather than imperative, programming.</li>
+        <li>React is a JavaScript library for building User Interfaces.</li>
+        <li>React uses a virtual DOM and compares that with the browser DOM to update the UI when needed.</li>
+        <li>React uses reusable components in its applications.</li>
+      </ol>
+      <AboutReact />
+    </div>
+  )
 };
 
 export default App;
